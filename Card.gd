de746e5c6a2cf0cf25a7115b577c9ddac781da23
Bill Marcy <wgmarcy@gmail.com>
2021-06-12 19:17:48 -0400

    Fix/refactor pair zone

--- Card.gd
+++ Card.gd
@@ -16,14 +16,16 @@ func _on_Card_input_event(viewport, event, shape_idx):
 		var overlapping =  get_overlapping_areas()
 		var point = event.position
 		var local_max_z = z_index
+		var in_pairzone = false
 		for area in overlapping:
+			if area.name == "PairZone":
+				in_pairzone = true
 			if area.get_script() == get_script() and area.z_index > local_max_z and area.is_colliding(point):
 				local_max_z = area.z_index
 		
 		if event.is_pressed():
 			if z_index == local_max_z:
 				get_parent().selected_card = self
-				print(get_parent().selected_card.name)
 				z_index = get_parent().max_z + 1
 
 		else: # released
@@ -31,7 +33,17 @@ func _on_Card_input_event(viewport, event, shape_idx):
 				if z_index < local_max_z:
 					z_index = local_max_z + 1
 				get_parent().max_z = max(z_index, get_parent().max_z)
-				get_parent().set_selected_card(null)
+				
+				if in_pairzone:
+					var card_to_be_paired = get_parent().get_node("PairZone").card_to_be_paired
+					
+					if card_to_be_paired:
+						print("pairing " + self.name + " with " + card_to_be_paired.name)
+						self.queue_free()
+						card_to_be_paired.queue_free()
+					else:
+						get_parent().get_node("PairZone").card_to_be_paired = self
+				get_parent().selected_card = null
 
 func is_colliding(point):
 	var rect = get_node('CollisionShape2D').get_shape()
--- Field.gd
+++ Field.gd
@@ -7,10 +7,6 @@ var selected_card = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
-	
-func set_selected_card(card):
-	yield(get_tree(), "idle_frame")
-	selected_card = card
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
